bd4885c0783d908bbb37d6a14abc4997569b9296
laohan <1807992307@qq.com>
2019-07-08 22:19:01 +0800

    bbbb

--- src/main/java/cn/itcast/git/gitdemo.java
+++ src/main/java/cn/itcast/git/gitdemo.java
@@ -3,7 +3,6 @@ package cn.itcast.git;
 public class gitdemo {
     public static void main(String[] args) {
         System.out.println(111);
-        System.out.println(222);
-        System.out.println(333);
+        System.out.println(444);
     }
 }
